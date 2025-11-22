01fef3fd6995caa46e15e61bc11f76c05bc7264d
Şahin KALEM <sahin-kalem@hotmail.com>
2025-11-22 22:19:22 +0300

    22.11.2025 22:15

--- app/src/main/java/sahinkalem/roomdao/Adapters/BookVolumeAdapter.java
+++ app/src/main/java/sahinkalem/roomdao/Adapters/BookVolumeAdapter.java
@@ -5,7 +5,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
